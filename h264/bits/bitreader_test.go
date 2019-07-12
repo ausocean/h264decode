79c9794862853508ef8b4e35fb25d360d1197d96
Saxon <saxon@ausocean.org>
2019-07-12 15:07:27 +0930

    h264/bits/bitreader_test.go: better naming and added field names to test tables

--- h264/bits/bitreader_test.go
+++ h264/bits/bitreader_test.go
@@ -19,52 +19,52 @@ import (
 // TestReadBits checks that BitReader.ReadBits behaves as expected.
 func TestReadBits(t *testing.T) {
 	tests := []struct {
-		in    []byte   // The bytes the source io.Reader will be initialised with.
-		reads []uint   // The values of n for the reads we wish to do.
-		wants []uint64 // The results we expect for each ReadBits call.
-		errs  []error  // The error expected from each ReadBits call.
+		in   []byte   // The bytes the source io.Reader will be initialised with.
+		n    []uint   // The values of n for the reads we wish to do.
+		want []uint64 // The results we expect for each ReadBits call.
+		err  []error  // The error expected from each ReadBits call.
 	}{
 		{
-			[]byte{0xff},
-			[]uint{8},
-			[]uint64{0xff},
-			[]error{nil},
+			in:   []byte{0xff},
+			n:    []uint{8},
+			want: []uint64{0xff},
+			err:  []error{nil},
 		},
 		{
-			[]byte{0xff},
-			[]uint{4, 4},
-			[]uint64{0x0f, 0x0f},
-			[]error{nil, nil},
+			in:   []byte{0xff},
+			n:    []uint{4, 4},
+			want: []uint64{0x0f, 0x0f},
+			err:  []error{nil, nil},
 		},
 		{
-			[]byte{0xff},
-			[]uint{1, 7},
-			[]uint64{0x01, 0x7f},
-			[]error{nil, nil},
+			in:   []byte{0xff},
+			n:    []uint{1, 7},
+			want: []uint64{0x01, 0x7f},
+			err:  []error{nil, nil},
 		},
 		{
-			[]byte{0xff, 0xff},
-			[]uint{8, 8},
-			[]uint64{0xff, 0xff},
-			[]error{nil, nil},
+			in:   []byte{0xff, 0xff},
+			n:    []uint{8, 8},
+			want: []uint64{0xff, 0xff},
+			err:  []error{nil, nil},
 		},
 		{
-			[]byte{0xff, 0xff},
-			[]uint{4, 8, 4},
-			[]uint64{0x0f, 0xff, 0x0f},
-			[]error{nil, nil, nil},
+			in:   []byte{0xff, 0xff},
+			n:    []uint{4, 8, 4},
+			want: []uint64{0x0f, 0xff, 0x0f},
+			err:  []error{nil, nil, nil},
 		},
 		{
-			[]byte{0xff, 0xff},
-			[]uint{16},
-			[]uint64{0xffff},
-			[]error{nil},
+			in:   []byte{0xff, 0xff},
+			n:    []uint{16},
+			want: []uint64{0xffff},
+			err:  []error{nil},
 		},
 		{
-			[]byte{0x8f, 0xe3},
-			[]uint{4, 2, 4, 6},
-			[]uint64{0x8, 0x3, 0xf, 0x23},
-			[]error{nil, nil, nil, nil},
+			in:   []byte{0x8f, 0xe3},
+			n:    []uint{4, 2, 4, 6},
+			want: []uint64{0x8, 0x3, 0xf, 0x23},
+			err:  []error{nil, nil, nil, nil},
 		},
 	}
 
@@ -72,21 +72,21 @@ func TestReadBits(t *testing.T) {
 		br := NewBitReader(bytes.NewReader(test.in))
 
 		// Holds the results from the reads.
-		gotReads := make([]uint64, len(test.reads))
+		got := make([]uint64, len(test.n))
 
 		// For each value of n defined in test.reads, we call br.ReadBits, collect
 		// the result and check the error.
 		var err error
-		for j, n := range test.reads {
-			gotReads[j], err = br.ReadBits(n)
-			if err != nil && errors.Cause(err) != test.errs[j] {
+		for j, n := range test.n {
+			got[j], err = br.ReadBits(n)
+			if err != nil && errors.Cause(err) != test.err[j] {
 				t.Fatalf("did not expect error: %v for read: %d test: %d", err, j, i)
 			}
 		}
 
 		// Now we can check the read results.
-		if !reflect.DeepEqual(gotReads, test.wants) {
-			t.Errorf("did not get expected results from ReadBits for test: %d\nGot: %v\nWant: %v\n", i, gotReads, test.wants)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("did not get expected results from ReadBits for test: %d\nGot: %v\nWant: %v\n", i, got, test.want)
 		}
 	}
 }
@@ -94,34 +94,34 @@ func TestReadBits(t *testing.T) {
 // TestPeekBits checks that BitReader.PeekBits behaves as expected.
 func TestPeekBits(t *testing.T) {
 	tests := []struct {
-		in    []byte
-		peeks []uint
-		wants []uint64
-		errs  []error
+		in   []byte
+		n    []uint
+		want []uint64
+		err  []error
 	}{
 		{
-			[]byte{0xff},
-			[]uint{8},
-			[]uint64{0xff},
-			[]error{nil},
+			in:   []byte{0xff},
+			n:    []uint{8},
+			want: []uint64{0xff},
+			err:  []error{nil},
 		},
 		{
-			[]byte{0x8f, 0xe3},
-			[]uint{4, 8, 16},
-			[]uint64{0x8, 0x8f, 0x8fe3},
-			[]error{nil, nil, nil},
+			in:   []byte{0x8f, 0xe3},
+			n:    []uint{4, 8, 16},
+			want: []uint64{0x8, 0x8f, 0x8fe3},
+			err:  []error{nil, nil, nil},
 		},
 		{
-			[]byte{0x8f, 0xe3, 0x8f, 0xe3},
-			[]uint{32},
-			[]uint64{0x8fe38fe3},
-			[]error{nil},
+			in:   []byte{0x8f, 0xe3, 0x8f, 0xe3},
+			n:    []uint{32},
+			want: []uint64{0x8fe38fe3},
+			err:  []error{nil},
 		},
 		{
-			[]byte{0x8f, 0xe3},
-			[]uint{3, 5, 10},
-			[]uint64{0x4, 0x11, 0x23f},
-			[]error{nil, nil, nil},
+			in:   []byte{0x8f, 0xe3},
+			n:    []uint{3, 5, 10},
+			want: []uint64{0x4, 0x11, 0x23f},
+			err:  []error{nil, nil, nil},
 		},
 	}
 
@@ -129,21 +129,21 @@ func TestPeekBits(t *testing.T) {
 		br := NewBitReader(bytes.NewReader(test.in))
 
 		// Holds the results from the peeks.
-		gotPeeks := make([]uint64, len(test.peeks))
+		got := make([]uint64, len(test.n))
 
 		// For each value of n defined in test.peeks, we call br.PeekBits, collect
 		// the result and check the error.
 		var err error
-		for j, n := range test.peeks {
-			gotPeeks[j], err = br.PeekBits(n)
-			if err != nil && errors.Cause(err) != test.errs[j] {
+		for j, n := range test.n {
+			got[j], err = br.PeekBits(n)
+			if err != nil && errors.Cause(err) != test.err[j] {
 				t.Fatalf("did not expect error: %v for peek: %d test: %d", err, j, i)
 			}
 		}
 
 		// Now we can check the peek results.
-		if !reflect.DeepEqual(gotPeeks, test.wants) {
-			t.Errorf("did not get expected results from PeekBits for test: %d\nGot: %v\nWant: %v\n", i, gotPeeks, test.wants)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("did not get expected results from PeekBits for test: %d\nGot: %v\nWant: %v\n", i, got, test.want)
 		}
 	}
 }
@@ -161,14 +161,14 @@ func TestReadOrPeek(t *testing.T) {
 		op   []int    // The series of operations we want to perform (read or peek).
 		n    []uint   // The values of n for the reads/peeks we wish to do.
 		want []uint64 // The results we expect for each ReadBits call.
-		errs []error  // The error expected from each ReadBits call.
+		err  []error  // The error expected from each ReadBits call.
 	}{
 		{
-			[]byte{0x8f, 0xe3, 0x8f, 0xe3},
-			[]int{read, peek, peek, read, peek},
-			[]uint{13, 3, 3, 7, 12},
-			[]uint64{0x11fc, 0x3, 0x3, 0x38, 0xfe3},
-			[]error{nil, nil, nil, nil, nil},
+			in:   []byte{0x8f, 0xe3, 0x8f, 0xe3},
+			op:   []int{read, peek, peek, read, peek},
+			n:    []uint{13, 3, 3, 7, 12},
+			want: []uint64{0x11fc, 0x3, 0x3, 0x38, 0xfe3},
+			err:  []error{nil, nil, nil, nil, nil},
 		},
 	}
 
@@ -190,7 +190,7 @@ func TestReadOrPeek(t *testing.T) {
 			default:
 				t.Fatalf("unrecognised operation requested")
 			}
-			if err != nil && errors.Cause(err) != test.errs[j] {
+			if err != nil && errors.Cause(err) != test.err[j] {
 				t.Fatalf("did not expect error: %v for operation: %d test: %d", err, j, i)
 			}
 		}
